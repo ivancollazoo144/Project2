2655ee0052b67d7ece194cce4716b757e0390fb5
ivancollazoo <collazoivan1412@gmail.com>
2025-01-29 22:10:01 -0400

    3 is the little one

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -3,7 +3,15 @@ package demo1;
 public class DemoTest2 {
 public static void main(String[] args) {
 		
+	int a = 3;
+	int b = 200;
+	
+	if (a < b) {
+		System.out.println("3 is the little one");
+	} else {
 		
-		System.out.println("Java");
+	
+	System.out.println("I dont know math");
+	}
 	}
 }
